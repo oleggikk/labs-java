28a50914d5672f4f3db86f80a20b71b07e5eb779
John Doe <oleg.y07.10.03@gmail.com>
2021-11-12 20:16:02 +0300

    done

---
+++ lab1/src/com/company/task4.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите номер месяца: ");
+        while ((!(console.hasNextInt())))  {
+            System.out.println("Число должно быть целым и быть в пределах от " + 1 + " до " + 12 + "\nПовторите ввод: ");
+            console.next();
+        }
+        int month = console.nextInt();
+
+        if ((month == 12) || (month == 1) || (month == 2)) {
+            System.out.println("Зима");
+        }
+        else if ((month == 3) || (month == 4) || (month == 5)) {
+            System.out.println("Весна");
+        }
+        else if ((month == 6) || (month == 7) || (month == 8)) {
+            System.out.println("Лето");
+        }
+        else if ((month == 9) || (month == 10) || (month == 11)) {
+            System.out.println("Осень");
+        }
+        else {
+            System.out.println("В году 12 месяцев(1-12, ни больше, ни меньше), в следующий раз у вас обязательно получится!");
+        }
+    }
+}
