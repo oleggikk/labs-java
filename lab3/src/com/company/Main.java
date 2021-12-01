f6ce7050e9c68a697746ccf2f4ffd778388bbfca
John Doe <oleg.y07.10.03@gmail.com>
2021-12-01 19:16:00 +0300

    commited

---
+++ lab3/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Введите дату в формате xx/mm/yyyy, учтите, что при вводе числа или месяца от 1 до 9 необходимо написать ноль перед числом или месяцем.\ndd/mm/yyyy: ");
+        Scanner console = new Scanner(System.in);
+        boolean result = checkDate(console.nextLine());
+        console.close();
+        if (result == true) {
+            System.out.println("Ваша строка имеет формат dd/mm/yyyy");
+        }
+        else {
+            System.out.println("Ваша строка не соответсвует формату dd/mm/yyyy");
+        }
+    }
+    public static boolean checkDate(String inputString) {
+        boolean checker;
+        String date = "^(0[1-9]|[12][0-9]|3[01])[-/.](0[1-9]|1[012])[-/.](1[9]\\d\\d|[2-9]\\d\\d\\d)$";
+        checker = inputString.matches(date);
+        return checker;
+    }
+}
