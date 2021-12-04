a0b5bbe050e57b349b2d188af9d48a52efca40e2
John Doe <oleg.y07.10.03@gmail.com>
2021-12-04 18:38:32 +0300

    commited

--- lab3/src/com/company/Main.java
+++ lab3/src/com/company/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Введите дату в формате xx/mm/yyyy, учтите, что при вводе числа или месяца от 1 до 9 необходимо написать ноль перед числом или месяцем.\ndd/mm/yyyy: ");
+        System.out.print("Введите дату в формате xx/mm/yyyy, учтите, что при вводе числа или месяца от 1 до 9 необходимо написать ноль перед числом или месяцем." +
+                "\ndd/mm/yyyy: ");
         Scanner console = new Scanner(System.in);
         boolean result = checkDate(console.nextLine());
         console.close();
@@ -16,9 +17,10 @@ public class Main {
             System.out.println("Ваша строка не соответсвует формату dd/mm/yyyy");
         }
     }
+
     public static boolean checkDate(String inputString) {
         boolean checker;
-        String date = "^(0[1-9]|[12][0-9]|3[01])[-/.](0[1-9]|1[012])[-/.](1[9]\\d\\d|[2-9]\\d\\d\\d)$";
+        String date = "^(0[1-9]|[12][0-9]|3[0])[-/.](0[2469]|1[1])|(0[1-9]|[12][0-9]|3[01])[-/.](0[13578]|1[02])[-/.](1[9]\\d\\d|[2-9]\\d\\d\\d)$";
         checker = inputString.matches(date);
         return checker;
     }
